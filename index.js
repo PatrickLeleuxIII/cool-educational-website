f59f3bfee40bbad857f0b448c8c447b4f7bd5fe0
PatrickLeleuxIII <103218727+PatrickLeleuxIII@users.noreply.github.com>
2024-03-18 14:15:50 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const array=["./1.jpg","./2.png","./3.png","./4.png","./5.png"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function buttonClicked(){
+  const elementNode = document.createElement('img');
+  const index=getRandomInt(5);
+  elementNode.setAttribute('src', array[index]);
+  document.body.appendChild(elementNode);
+  
+}
